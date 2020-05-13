0aae0a80c368413721dba5cc1087d2613cc8c68b
Sayid Miqdad <sayidmiqdad@gmail.com>
2020-05-13 15:01:09 +0700

    Tugas Akhir

---
+++ src/ta_dkp/Submit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ta_dkp;
+
+/**
+ *
+ * @author Sayid
+ */
+public class Submit {
+    static String cetak() 
+        {
+        return "Transaksi Berhasil";
+        }
+    static String cetak1() 
+        {
+        return "MMR anda akan naik dalam 1 minggu";
+        }
+}
---
+++ src/ta_dkp/TA_DKP.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ta_dkp;
+
+/**
+ *
+ * @author Sayid
+ */
+public class TA_DKP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
